bc08da3dc3882b13c8f318a18b9bc26f93669c34
Andrey Savihin <andrey.savihin@onlyoffice.com>
2018-08-10 12:22:34 +0300

    Copyright 2018

--- web/studio/ASC.Web.Studio/Products/Projects/Core/Model/DataInterfaces/IDaoFactory.cs
+++ web/studio/ASC.Web.Studio/Products/Projects/Core/Model/DataInterfaces/IDaoFactory.cs
@@ -1,6 +1,6 @@
 /*
  *
- * (c) Copyright Ascensio System Limited 2010-2016
+ * (c) Copyright Ascensio System Limited 2010-2018
  *
  * This program is freeware. You can redistribute it and/or modify it under the terms of the GNU 
  * General Public License (GPL) version 3 as published by the Free Software Foundation (https://www.gnu.org/copyleft/gpl.html). 
@@ -22,7 +22,6 @@
  * Pursuant to Section 7 § 3(e) we decline to grant you any rights under trademark law for use of our trademarks.
  *
 */
-
 
 
 namespace ASC.Projects.Core.DataInterfaces
